7b8fc65b9fefa892a3e85fbdf2b69b33b48ece07
Patrick Tappe <patrick.tappe@riskfocus.com>
2022-09-19 20:41:50 +0000

    Added types.ts to represent data structure

---
+++ src/types/types.ts
@@ -0,0 +1,31 @@
+export type Agent = {
+    login: {
+        username: string;
+        uuid: string;
+    }
+    name: {
+        first: string;
+        last: string;
+    }
+    location: {
+        city: string;
+        state: string;
+        country: string;
+        coordinates: {
+            longitude: string;
+            latitude: string;
+        }
+        timezone: {
+            offset: string;
+            description: string;
+        }
+        dob: {
+            age: string;
+            date: string;
+        }
+        picture: {
+            large: string;
+        }
+        gender: string;
+    }
+}
